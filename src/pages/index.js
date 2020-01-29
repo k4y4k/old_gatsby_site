a39a04af71e5a45f13446f4efd82d660152a54c5
kayak <kayak@kayak.rocks>
2020-01-29 23:04:31 +1100

    chore: rerun prettier, other last-minute fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
-import React from "react"
-import "../global.css"
-import Particles from "react-particles-js"
-import { Helmet } from "react-helmet"
+import React from 'react'
+import '../global.css'
+import Particles from 'react-particles-js'
+import { Helmet } from 'react-helmet'
 
 export default () => (
   <>
     <Helmet>
       <html lang="en" amp />
       <meta charSet="utf-8" />
-      <title>{"Coming Soon | <kayak />"}</title>
+      <title>{'Coming Soon | <kayak />'}</title>
       <link rel="canonical" href="http://mysite.com/example" />
     </Helmet>
     <Particles className="particles" />
@@ -16,8 +16,8 @@ export default () => (
       <div id="content">
         <p>Coming soon: your new favourite web developer and designer</p>
         <h1>
-          <span className="fade">{"<"}</span>kayak
-          <span className="fade">{"/>"}</span>
+          <span className="fade">{'<'}</span>kayak
+          <span className="fade">{'/>'}</span>
         </h1>
       </div>
     </div>
