48a8c3f77689fc3988dee31e489ac8d9379f246e
kayak <kayak@kayak.rocks>
2020-01-29 21:37:23 +1100

    feat: coming soon page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,19 @@
 import React from "react"
+import "../global.css"
+import Particles from "react-particles-js"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <>
+    <Particles className="particles" />
+    <div id="container">
+      <div id="content">
+        <p>Coming soon: your new favourite web developer and designer</p>
+        <h1>
+          <span className="fade">{"<"}</span>kayak
+          <span className="fade">{"/>"}</span>
+        </h1>
+        {/* <Particles /> */}
+      </div>
+    </div>
+  </>
+)
