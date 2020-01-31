18738c501f0c6a118e9405c01c09df1278228597
kayak <kayak@kayak.rocks>
2020-01-31 18:22:15 +1100

    build: add purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,15 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-plugin-postcss`,
     {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true,
+        develop: process.env.NODE_ENV === 'development' ? true : false,
+        tailwind: true,
+        verbose: true
+      }
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: '<kayak />',
