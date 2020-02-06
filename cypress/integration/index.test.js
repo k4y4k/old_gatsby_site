f6727d2b2dbc73bcb272ebc8c70419d49a1aa81f
kayak <kayak@kayak.rocks>
2020-02-06 13:10:40 +1000

    chore: update button component / tests

--- cypress/integration/index.test.js
+++ cypress/integration/index.test.js
@@ -1,22 +1,22 @@
-/// <reference types="Cypress" />
+// / <reference types="Cypress" />
 
 describe('The site in general', () => {
   it('loads with 0 tanties', () => {
-    cy.visit('/')
-  })
+    cy.visit('/');
+  });
 
   it('has a projects section', () => {
-    cy.get('#projects')
-  })
+    cy.get('#projects');
+  });
 
   it('has a blog section', () => {
-    cy.get('#blog')
-  })
-})
+    cy.get('#blog');
+  });
+});
 
-describe('Blog Section', function() {
+describe('Blog Section', () => {
   it('has a featured post, with a title, an image, and a link to read it', () => {
-    cy.get('#featured-blog-post').then(post => {
+    cy.get('#featured-blog-post').then((post) => {
       cy.wrap(post)
         .find('span')
         .parent()
@@ -26,30 +26,27 @@ describe('Blog Section', function() {
         .parent()
         .find('p')
         .parent()
-        .find('a')
-    })
-  })
+        .find('a');
+    });
+  });
 
-describe("Blogroll",()=>{
-
-it("exists and has at least one child",()=>{
-cy.get("#blogroll").children().should("have.length.gte",1) 
-
- })
-
-})
-
-
-})
+  describe('Blogroll', () => {
+    it('exists and has at least one child', () => {
+      cy.get('#blogroll')
+        .children()
+        .should('have.length.gte', 1);
+    });
+  });
+});
 
 describe('Projects', () => {
   it('has at least 1 visible project', () => {
-    cy.get('.project-card')
-  })
+    cy.get('.project-card');
+  });
 
   describe('Project cards', () => {
     it('should have an image, a title, a desciption', () => {
-      cy.get('.project-card').then(x => {
+      cy.get('.project-card').then((x) => {
         // FIXME: probably better way to do this
         cy.wrap(x)
           .find('img')
@@ -58,64 +55,61 @@ describe('Projects', () => {
           .parent()
           .find('h2')
           .parent()
-          .find('p')
-      })
-    })
+          .find('p');
+      });
+    });
 
     it('should have one or more buttons that go to some code or a live demo or both', () => {
-      cy.get('.project-card').then(x => {
+      cy.get('.project-card').then((x) => {
         cy.wrap(x)
           .find('a')
-          .then(y => {
-            cy.wrap(y)
-              .should('have.attr', 'href')
-              .then(href => {
-                cy.visit(href)
-              })
-          })
-      })
-    })
-  })
-})
+          .then((y) => {
+            cy.wrap(y).should('have.attr', 'href');
+          });
+      });
+    });
+  });
+});
 
 describe('Hero', () => {
   it('has a hella cool title', () => {
-    cy.get('img')
-  })
+    cy.visit('/');
+    cy.get('img');
+  });
 
   describe('the nav', () => {
     it('has links', () => {
       cy.get('#hero-nav > ul')
         .children()
-        .should('have.length.gte', 0)
-    })
+        .should('have.length.gte', 0);
+    });
 
     it('has a hire button as the last link', () => {
-      cy.get('li:last-of-type').then(lastEl => {
+      cy.get('li:last-of-type').then((lastEl) => {
         cy.wrap(lastEl)
           .contains(/hire/i)
-          .and('has.css', 'background-color')
-      })
-    })
+          .and('has.css', 'background-color');
+      });
+    });
 
     it('changes on hover', () => {
       cy.get('li')
-        .each(listitem => {
-          listitem.trigger('mouseover')
+        .each((listitem) => {
+          listitem.trigger('mouseover');
         })
         .should('have.css', 'text-decoration')
-        .and('match', /solid/)
-    })
-  })
-})
+        .and('match', /solid/);
+    });
+  });
+});
 
 describe('Accessibility checks', () => {
   beforeEach(() => {
-    cy.visit('/')
-    cy.injectAxe()
-    cy.wait(500)
-  })
+    cy.visit('/');
+    cy.injectAxe();
+    cy.wait(500);
+  });
   it('Has no detectable a11y violations on load', () => {
-    cy.checkA11y()
-  })
-})
+    cy.checkA11y();
+  });
+});
--- src/components/general/Button.js
+++ src/components/general/Button.js
@@ -2,12 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Button = ({ children }) => (
-  <button
-    type="button"
+  <a
+    href="https://example.com"
     className="uppercase font-mono text-purple-600 p-2 m-1 border-purple-600 border-solid border inline-block self-start"
   >
     {children}
-  </button>
+  </a>
 );
 
 const StyledButton = styled(Button)``;
